a17177204941e4cd46ad7fa750d28276569e4350
Dylan Armstrong <dylan@Dylans-MacBook-Pro.local>
2023-06-03 09:42:23 +1000

    Updated some stuff in main typescript file

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import router from "./router/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import auth from "./auth/auth";
 
-const app = createApp(App);
-app.config.globalProperties.$isAuthenticated = false;
-app.use(router).mount("#app");
+createApp(App).use(router).mount("#app");
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some((record) => record.meta.requiresAuth)) {
